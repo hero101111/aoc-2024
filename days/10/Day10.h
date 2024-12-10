afce719fbccd77961250c04d6d5dc42ed142224b
Gabriel Diaconita <diaconitagabriel@gmail.com>
2024-12-10 07:36:43 +0200

    Add Day10 solution

--- days/10/Day10.h
+++ days/10/Day10.h
@@ -20,15 +20,52 @@ public:
 
   string GetDay() override { return "10"; }
 
-  LL DoWork1()
+  LL DoWork(bool partOne)
   {
     LL ret = 0;
-    return ret;
-  }
+    Graph<Point>    g(mData[0].size() * mData[0].size());
+    DynamicMap<char> m;
 
-  LL DoWork2()
-  {
-    LL ret = 0;
+    vector<Point> heads, trails;
+    m.fromfile(GetInputPath());
+    m.for_each(
+      [&](Point p, char c) {
+        if (c == '0')
+          heads.push_back(p);
+        if (c == '9')
+          trails.push_back(p);
+        int myVal = c - '0';
+
+        auto neigh = p.GetDirectNeighbours();
+        for (auto n : neigh)
+        {
+          if (m.isSetAt(n))
+          {
+            int otherVal = m[n] - '0';
+            if (otherVal == myVal + 1)
+              g.AddEdge(p, n, 1);
+          }
+        }
+      });
+    for (auto h : heads)
+    {
+      auto dist = g.GetDistances(h);
+      for (auto t : trails)
+      {
+          if (dist[t] != numeric_limits<int>::max() - 1)
+          {
+            if (partOne)
+            {
+              ret += 1;
+            }
+            else
+            {
+              auto paths = g.GetAllPaths(h, t);
+              ret += paths.size();
+            }
+          }
+      }
+    }
     return ret;
   }
 
@@ -36,20 +73,20 @@ public:
   {
     ReadData();
 
-    return std::to_string(DoWork1());
+    return std::to_string(DoWork(true));
   }
 
   string Part2() override
   {
     ReadData();
 
-    return std::to_string(DoWork2());
+    return std::to_string(DoWork(false));
   }
 
   bool Test() override
   {
     mCurrentInput = "test";
-    // assert(Part1() != "");
+     assert(Part1() != "");
     // assert(Part2() != "");
     return true;
   }
