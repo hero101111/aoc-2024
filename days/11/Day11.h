2159ad071e6b3541ef719946139db2c73c20dbef
Gabriel Diaconita <diaconitagabriel@gmail.com>
2024-12-11 11:17:27 +0200

    xx

--- days/11/Day11.h
+++ days/11/Day11.h
@@ -23,6 +23,39 @@ public:
   LL DoWork1()
   {
     LL ret = 0;
+    auto stonesData = stoll(tok(mData[0], ' '));
+    list<LL> stones;
+    for (auto s : stonesData)
+      stones.insert(end(stones), s);
+
+    for (int i = 0; i < 75; ++i)
+    {
+      cout << i << "        ";
+      list<LL> stones2;
+      for (auto s : stones)
+      {
+        if (s == 0)
+        {
+          stones2.push_back(1);
+          continue;
+        }
+        auto sstr = to_string(s);
+        if (sstr.size() % 2 == 0)
+        {
+          LL s1 = stoll(sstr.substr(0, sstr.size() / 2));
+          LL s2 = stoll(sstr.substr(sstr.size() / 2));
+          stones2.push_back(s1);
+          stones2.push_back(s2);
+        }
+        else
+        {
+          stones2.push_back(s * 2024);
+        }
+      }
+      stones = stones2;
+      cout << stones.size() << endl;
+    }
+    ret = stones.size();
     return ret;
   }
 
